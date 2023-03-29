8126abbd6352e4abe4c53e614c454f70b5a94151
Arina Fazal <arinafazal95@gmail.com>
2023-03-29 21:31:16 +0700

    First Commit

---
+++ practice.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = '93a4938f-1302-4449-b573-41aa134080c0'
+word = 'potato'
+url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}'
+
+res = requests.get(url)
+
+definitions = res.json()
+
+for definition in definitions:
+    print(definition)
